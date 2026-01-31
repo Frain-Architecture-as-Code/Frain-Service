3b13398b8ab0841ea3c77ef7f9d50ba6228f970d
JonatanFD <u20211b995@upc.edu.pe>
2026-01-31 01:57:10 -0500

    feat: first commit

---
+++ src/main/java/com/frain/frainapi/FrainApplication.java
@@ -0,0 +1,13 @@
+package com.frain.frainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrainApplication.class, args);
+    }
+
+}
